a5807122ceb68a349426b7b15608abf068c58f09
App Academy Student <appacademy@Mac-mini.local>
2018-12-04 13:33:09 -0800

    Implement Root component and start on selectors

---
+++ todo/frontend/reducers/selectors.js
@@ -0,0 +1,5 @@
+export const allTodos = (state) => {
+  let keys = Object.keys(state.todos);
+  const todos = [];
+  keys.forEach()
+};
--- todo/frontend/todo_redux.jsx
+++ todo/frontend/todo_redux.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import App from './components/app';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   
   ReactDOM.render(
-    <App />,
+    <Root />,
     root
   );
 });
